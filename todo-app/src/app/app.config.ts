8526c68ea4bc63b945e65fe60b996b5b0df620f9
bendesbeus <bendesbeatrix@gmail.com>
2024-03-05 18:59:45 +0100

    create ngrx

--- todo-app/src/app/app.config.ts
+++ todo-app/src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
-
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
+import { todoStore } from './todo/store/todo.reducers';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideStore(),
+    provideStore({ todo: todoStore },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true
+        }
+      }
+    ),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
